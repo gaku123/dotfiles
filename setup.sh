99efcb43b0e4c38bdff570ec86984d6fdb6786d7
Gaku Yasui <yasui.gaku@H-990-GAKUYASUI.a-tm.local>
2017-02-20 00:02:46 +0900

    強制的にファイルを作成する

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ DOT_FILES=(.bashrc .screenrc .gitconfig .git-completion.bash .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  ln -fs $HOME/dotfiles/$file $HOME/$file
 done
 
 # vim
-mkdir $HOME/.vim/tmp
+mkdir -p $HOME/.vim/tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
